c07e2affd2aea37e85da8157663b5c5c00fe3b2c
Gonzalo <gonza_219@hotmail.com>
2018-02-06 01:05:48 -0300

    Initial Commit

---
+++ Swift_Practice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift_Practice
+//
+//  Created by Gonzalo Silva on 06/02/2018.
+//  Copyright © 2018 Gonzalo Silva. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
